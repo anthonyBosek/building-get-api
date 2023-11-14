f2eedbee9c06ce3b1e7e96251f2f0358af1ccc28
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-13 18:03:25 -0700

    complete lab

--- server/seed.py
+++ server/seed.py
@@ -11,7 +11,6 @@ from models import db, Game, Review, User
 fake = Faker()
 
 with app.app_context():
-
     Review.query.delete()
     User.query.delete()
     Game.query.delete()
@@ -24,44 +23,35 @@ with app.app_context():
     db.session.add_all(users)
 
     games = []
-    games.append(Game(
-        title='Mega Adventure',
-        genre='Survival',
-        platform='XBox',
-        price=30))
-    games.append(Game(
-        title='Golf Pro IV',
-        genre='Sports',
-        platform="PlayStation",
-        price=20))
-    games.append(Game(
-        title='Dance, dance, dance',
-        genre='Party',
-        platform="PlayStation",
-        price=7))
+    games.append(
+        Game(title="Mega Adventure", genre="Survival", platform="XBox", price=30)
+    )
+    games.append(
+        Game(title="Golf Pro IV", genre="Sports", platform="PlayStation", price=20)
+    )
+    games.append(
+        Game(
+            title="Dance, dance, dance", genre="Party", platform="PlayStation", price=7
+        )
+    )
     db.session.add_all(games)
 
     reviews = []
-    reviews.append(Review(
-        score=9,
-        comment='Amazing action',
-        user=users[0],
-        game=games[0]))
-    reviews.append(Review(
-        score=2,
-        comment='Boring',
-        user=users[0],
-        game=games[1]))
-    reviews.append(Review(
-        score=5,
-        comment='Not enough levels',
-        user=users[1],
-        game=games[0]))
-    reviews.append(Review(
-        score=randint(0, 10),
-        comment='confusing instructions',
-        user=users[2],
-        game=games[2]))
+    reviews.append(
+        Review(score=9, comment="Amazing action", user=users[0], game=games[0])
+    )
+    reviews.append(Review(score=2, comment="Boring", user=users[0], game=games[1]))
+    reviews.append(
+        Review(score=5, comment="Not enough levels", user=users[1], game=games[0])
+    )
+    reviews.append(
+        Review(
+            score=randint(0, 10),
+            comment="confusing instructions",
+            user=users[2],
+            game=games[2],
+        )
+    )
     db.session.add_all(reviews)
 
     for g in games:
